931f3c2e84604ec4abd70ab3aa4802929e4cf676
LaryStef <timurkotov1999@gmail.com>
2024-11-24 21:18:52 +0300

    refactor: type annotations

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-from typing import Union
-
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
 @app.get("/")
-def read_root():
+def read_root() -> dict[str, str]:
     return {"Hello": "World"}
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+def read_item(
+    item_id: int,
+    query: str | None = None
+) -> dict[str, str | int | None]:
+    return {"item_id": item_id, "query": query}
